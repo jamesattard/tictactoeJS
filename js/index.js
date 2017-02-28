dd06909f6cf7e376f8dd793430cd4e9378b337bc
James Attard <info@jamesattard.com>
2017-02-28 19:40:44 +0100

    Decided to ditch canvas

--- js/index.js
+++ js/index.js
@@ -1,25 +1,4 @@
 $(document).ready(function() {
 
-  window.onload = function() {
-    ctx = $('#canvas')[0].getContext("2d");
-    draw();
-  }
-
-  // Start Canvas Configuration
-  var canvasW = $("#canvas").width();
-  var canvasH = $("#canvas").height();;
-  var boardBg = new Image();
-  var boardX = new Image();
-  var boardO = new Image();
-
-  boardBg.src = "img/ttt_board.png";
-  boardX.src = "img/ttt_x.png";
-  boardO.src = "img/ttt_o.png";
-  // End Canvas Configuration
-
-  function draw() {
-    ctx.clearRect(0, 0, canvasW, canvasH);
-    ctx.drawImage(boardBg, 0, 0);
-  }
-
-}); //end of DRF
+  
+});
