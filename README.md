6667819486b2f3a035ab4a4404d31257fbbc5b2d
James Attard <info@jamesattard.com>
2017-03-02 09:32:11 +0100

    Added X-O choice through modal

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Tic-Tac-Toe game written in JS, JQuery, HTML, Bootstrap, CSS as part of the FreeCodeCamp advanced front-end development project curriculum. This game can be accessed by following the link below:
+Tic-Tac-Toe game written in JS, JQuery, HTML, Bootstrap, CSS as part of the FreeCodeCamp advanced front-end development project curriculum. I took a quick approach to complete this project (~1.5h) and I might refactor it to optimize algorithm to check win conditions and to add min-max AI algorithm
+
+This game can be accessed by following the link below:
 
 1. [Tic Tac Toe](https://jamesattard.github.io/tictactoeJS)  
--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@ $(document).ready(function() {
 
   var x = "X";
   var o = "O";
-  var human = x;
-  var ai = o;
+  var human, ai, available;
   var cells = [
     $('#cell0'), $('#cell1'), $('#cell2'),
     $('#cell3'), $('#cell4'), $('#cell5'),
     $('#cell6'), $('#cell7'), $('#cell8')
   ];
   var turns = 0;
-  var available;
 
   function checkLine(char, spot1, spot2, spot3) {
     if (cells[spot1].text() == char && cells[spot2].text() == char && cells[spot3].text() == char) {
+      cells[spot1].css("background-color", "white");
+      cells[spot2].css("background-color", "white");
+      cells[spot3].css("background-color", "white");
       return true;
     } else {
       return false;
@@ -24,24 +25,42 @@ $(document).ready(function() {
     if (checkLine(player, 0, 1, 2)) return true;
     if (checkLine(player, 3, 4, 5)) return true;
     if (checkLine(player, 6, 7, 8)) return true;
-
     if (checkLine(player, 0, 3, 6)) return true;
     if (checkLine(player, 1, 4, 7)) return true;
     if (checkLine(player, 2, 5, 8)) return true;
-
     if (checkLine(player, 0, 4, 8)) return true;
     if (checkLine(player, 2, 4, 6)) return true;
   }
 
-  function clearCells() {
+  function init() {
     for (var i=0; i<cells.length; i++) {
       cells[i].text('');
       cells[i].removeClass(human);
       cells[i].removeClass(ai);
+      cells[i].css("background-color", "#333");
     }
     turns = 0;
+    $('#myModal').modal('show');
   }
 
+  init();
+
+  // Choose X or Y
+  $(".select-side").click(function(event) {
+      event.preventDefault();
+      human = $(this).text();
+      $('#myModal').modal('hide');
+      human == x ? ai = o : ai = x;
+      if (human == o) {
+        // AI's turn (random mode)
+        locAI = Math.floor(Math.random() * 9); // AI choose cell 0-8
+        cells[locAI].text(ai);
+        cells[locAI].addClass(ai);
+        turns++;
+      }
+   });
+
+  // Game starts
   $('.tttBox').click(function() {
     if ($(this).text() != x && $(this).text() != o && turns < 9) {
       // if cell is not occupied by Human/AI, fill it
@@ -51,7 +70,7 @@ $(document).ready(function() {
       if (checkWin(human)) {
         setTimeout(function(){ // delay so we can see winning line
           alert('Human Wins!');
-          clearCells();
+          init();
         }, 250);
       };
 
@@ -70,20 +89,20 @@ $(document).ready(function() {
           if (checkWin(ai)) {
             setTimeout(function(){ // delay so we can see winning line
               alert('AI Wins!');
-              clearCells();
+              init();
             }, 250);
           };
         }
       }, 250);
 
-    } else {
+    } else if (turns >= 9) {
       alert("It's a tie!");
-      clearCells();
+      init();
     }
   }); // end of OCF
 
   $('#resetBtn').click(function() {
-    clearCells();
+    init();
   });
 
 }); // end of DRF
